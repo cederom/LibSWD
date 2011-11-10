d7cba5f13c63bdcb7552b9132a051e555736379a
Tomek CEDRO <cederom@tlen.pl>
2011-11-10 00:15:30 +0000

    Updated swd_init() to produce log information on using libswd.

--- src/libswd_core.c
+++ src/libswd_core.c
@@ -37,6 +37,7 @@
 /** \file libswd_core.c */
 
 #include <libswd.h>
+#include <config.h>
 
 /*******************************************************************************
  * \defgroup swd_core Library and Context (de)initialization routines.
@@ -66,6 +67,7 @@ swd_ctx_t *swd_init(void){
  swdctx->config.trnlen=SWD_TURNROUND_DEFAULT_VAL;
  swdctx->config.maxcmdqlen=SWD_CMDQLEN_DEFAULT;
  swdctx->config.loglevel=SWD_LOGLEVEL_NORMAL;
+ swd_log(swdctx, SWD_LOGLEVEL_NORMAL, "SWD_N: Using " PACKAGE_STRING " (http://libswd.sf.net)\nSWD_N: (c) Tomasz Boleslaw CEDRO (http://www.tomek.cedro.info)\n");
  return swdctx;
 }
 
