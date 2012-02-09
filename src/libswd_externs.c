ff81220cb1fbd819938286153950d9ca1ea0c5b1
Tomek CEDRO <cederom@tlen.pl>
2012-02-09 17:44:18 +0100

    Minor update of dates (2010-2012) in copyrights information.

--- src/libswd_externs.c
+++ src/libswd_externs.c
@@ -4,7 +4,7 @@
  * Serial Wire Debug Open Library.
  * External Handlers Definition File.
  *
- * Copyright (C) 2010-2011, Tomasz Boleslaw CEDRO (http://www.tomek.cedro.info)
+ * Copyright (C) 2010-2012, Tomasz Boleslaw CEDRO (http://www.tomek.cedro.info)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,7 +30,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.*
  *
- * Written by Tomasz Boleslaw CEDRO <cederom@tlen.pl>, 2010-2011;
+ * Written by Tomasz Boleslaw CEDRO <cederom@tlen.pl>, 2010-2012;
  *
  */
 
