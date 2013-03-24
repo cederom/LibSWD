4f772a9157e6d9c2df36cdaae748a7c8b2880847
Tomek CEDRO <cederom@tlen.pl>
2013-03-24 21:20:21 +0100

    CLI: Added LogLevel handling.

--- src/libswd_cli.c
+++ src/libswd_cli.c
@@ -36,6 +36,7 @@
 
 #include <libswd.h>
 #include <string.h>
+#include <errno.h>
 
 /*******************************************************************************
  * \defgroup libswd_cli Command Line Interface functions.
@@ -63,6 +64,7 @@ int libswd_cli(libswd_ctx_t *libswdctx, char *command){
  if (libswdctx==NULL) return LIBSWD_ERROR_NULLCONTEXT;
  if (command==NULL) return LIBSWD_ERROR_NULLPOINTER;
 
+ int retval;
  char *cmd;
  cmd=strtok(command," ");
  while (cmd!=NULL){
@@ -70,8 +72,39 @@ int libswd_cli(libswd_ctx_t *libswdctx, char *command){
        strncmp(cmd,"help",4)==0 || \
        strncmp(cmd,"h",1)==0) {
    return libswd_cli_print_usage(libswdctx); 
+
+  } else if ( strncmp(cmd,"l",1)==0 || strncmp(cmd,"loglevel",8)==0 ){
+   int loglevel_new=LIBSWD_LOGLEVEL_DEFAULT;
+   cmd=strtok(NULL," ");
+   if (cmd==NULL) {
+    libswd_log(libswdctx, LIBSWD_LOGLEVEL_INFO, "LIBSWD_I: Current loglevel is: %d (%s)\n", \
+    (int)libswdctx->config.loglevel, \
+    libswd_log_level_string(libswdctx->config.loglevel));
+   } else {
+    errno=LIBSWD_OK;
+    loglevel_new=atol(cmd);
+    if (errno==LIBSWD_OK){
+     retval=libswd_log_level_set(libswdctx, (libswd_loglevel_t)loglevel_new);
+     if (retval<0) {
+      libswd_log(libswdctx, LIBSWD_LOGLEVEL_ERROR, \
+                 "LIBSWD_E: libswd_cli(@libswdctx=%p, loglevel=%d/%s): %s\n", \
+                 (void*)libswdctx, loglevel_new, \
+                 libswd_log_level_string((libswd_loglevel_t)loglevel_new), \
+                 libswd_error_string(retval) );
+     }
+     cmd=strtok(NULL,command);
+    } else if (errno!=LIBSWD_OK){
+     libswd_log(libswdctx, LIBSWD_LOGLEVEL_INFO, "LIBSWD_I: Current loglevel is: %d (%s)\n", \
+      (int)libswdctx->config.loglevel, \
+      libswd_log_level_string(libswdctx->config.loglevel));
+    }
+   }
+   libswd_log(libswdctx, LIBSWD_LOGLEVEL_NORMAL, "LOGLEVEL=%d\n", (int)libswdctx->config.loglevel);
+   continue;
+
   } else if ( strncmp(cmd,"r",1)==0 || strncmp(cmd,"read",4)==0 ){
    printf("LIBSWD READ\n");
+
   } else {
    libswd_log(libswdctx, LIBSWD_LOGLEVEL_ERROR, "LIBSWD_E: libswd_cli(@libswdctx=%p, @command=%p): %s\n", (void*)libswdctx, (void*)command, libswd_error_string(LIBSWD_ERROR_CLISYNTAX));
    libswd_cli_print_usage(libswdctx);
