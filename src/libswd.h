e4fd987fc32a2f17394e70c194077e85d7ed1424
AndrewP <andrewp@carallon.com>
2015-04-10 14:50:02 +0100

    TARGET: Added support Cortex M0 based chips.

--- src/libswd.h
+++ src/libswd.h
@@ -744,6 +744,7 @@ static const libswd_arm_register_t libswd_arm_debug_CortexM3_SCS_ComponentID[] =
 static const libswd_arm_register_t libswd_arm_debug_CPUID[] = {
  { .address=0xE000ED00, .name="ARM Cortex-M3 r1p2",     .default_value=0x411FC231 },
  { .address=0xE000ED00, .name="ARM Cortex-M3 r2p1",     .default_value=0x412FC231 }, 
+ { .address=0xE000ED00, .name="ARM Cortex-M0 r0p0",     .default_value=0x410CC200 },
  0
 };
 
