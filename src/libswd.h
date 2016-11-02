60a3a977888611bb105057fc1f194b332194602b
Andrew Parlane <andrew.parlane@gmail.com>
2016-11-02 15:04:31 -0400

    TARGET: Changed the CPUID array terminator to be an actual entry.
    Previously the terminator was 0, however my compiler didn't support
    that.
    
    This array is only used in one place, and it is looped over until the address field is 0. So my change of just setting the address field to 0 for the terminator entry, works fine.

--- src/libswd.h
+++ src/libswd.h
@@ -747,7 +747,7 @@ static const libswd_arm_register_t libswd_arm_debug_CPUID[] = {
  { .address=0xE000ED00, .name="ARM Cortex-M3 r2p1",     .default_value=0x412FC231 }, 
  { .address=0xE000ED00, .name="ARM Cortex-M0 r0p0",     .default_value=0x410CC200 },
  { .address=0xE000ED00, .name="ARM Cortex-M4 r0p1",     .default_value=0x410FC241 },
- 0
+ { .address=0,          .name=""                  ,     .default_value=0 },
 };
 
 typedef struct libswd_debug {
