52fefeba9b28a25abddb5ed844136ba93dc97573
Andrew Parlane <andrewp@carallon.com>
2017-02-15 18:33:02 +0000

    Adding support for Cortex-M0 r0p0

--- src/libswd.h
+++ src/libswd.h
@@ -746,6 +746,7 @@ static const libswd_arm_register_t libswd_arm_debug_CPUID[] = {
  { .name="ARM Cortex-M3 r1p2",     .default_value=0x411FC231 },
  { .name="ARM Cortex-M3 r2p1",     .default_value=0x412FC231 },
  { .name="ARM Cortex-M0 r0p0",     .default_value=0x410CC200 },
+ { .name="ARM Cortex-M0+ r0p0",    .default_value=0x410CC600 },
  { .name="ARM Cortex-M4 r0p1",     .default_value=0x410FC241 },
 };
 
